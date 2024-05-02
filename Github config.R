174fe69e64840c46f599f7211b57e3fed9e304cb
Julien Delmas <murgen123@hotmail.com>
2024-05-02 19:54:44 +0200

    change CV  02052024 1

--- Github config.R
+++ Github config.R
@@ -5,18 +5,24 @@ library(usethis)
 
 usethis::use_git()
 
+#
 usethis::use_git_config(
     user.name = "Julien Delmas", 
     user.email = "murgen123@hotmail.com"
 )
 
+# create token on github
 usethis::create_github_token()
+
+#register token 
 gitcreds::gitcreds_set()
 
+#check token and everything was registered
 usethis::git_sitrep()
 
 
 usethis::use_github()
+2
 
 
 
